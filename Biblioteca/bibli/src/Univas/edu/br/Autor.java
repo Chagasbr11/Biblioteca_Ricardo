53399e46f16ec520ef08ee5b37ff4d301a3ea3e3
Chagasbr11 <114256601+Chagasbr11@users.noreply.github.com>
2023-10-09 23:28:55 -0300

    Add files via upload

---
+++ Biblioteca/bibli/src/Univas/edu/br/Autor.java
@@ -0,0 +1,10 @@
+package Univas.edu.br;
+
+public class Autor {
+
+    String nome;
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Biblioteca/bibli/src/Univas/edu/br/Main.java
@@ -0,0 +1,27 @@
+package Univas.edu.br;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca("Bibli");
+        Livro l1 = new Livro("3Porquinhos", "Maicon", false);
+        Livro l2 = new Livro("Futebol", "Leonardo", false);
+        Livro l3 = new Livro("Volei", "Maicon", false);
+
+        Autor a1 = new Autor("Carlos");
+        Autor a2 = new Autor("Henrique");
+        Autor a3 = new Autor("Vieira");
+
+        biblioteca.adicionarLivro(l1);
+        biblioteca.adicionarLivro(l2);
+
+        biblioteca.adicionarAutor(a1);
+        biblioteca.adicionarAutor(a2);
+        biblioteca.adicionarAutor(a3);
+
+        System.out.println(biblioteca.verificarDisponibilidade("3Porquinhos"));
+        System.out.println(biblioteca.verificarDisponibilidade("Futebol"));
+        System.out.println(biblioteca.verificarDisponibilidade("Volei"));
+    }
+}
